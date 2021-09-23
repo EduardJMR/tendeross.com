88f682c57744366e5133006299d831e71581a995
EduardJMR <eduardo.martinezrincon@gmail.com>
2021-09-23 18:43:18 -0500

    Config Database

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://kbalvhbvezzejl:f8530c879cf80bf6fbeb4cb24f63faaa470ea956c533efdf1533924b942997cc@ec2-3-219-111-26.compute-1.amazonaws.com:5432/dfiftl9067ie8d'
 
 @app.route('/')
 def home():
